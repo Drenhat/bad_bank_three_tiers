5e54eea00da199807c5b71fc140eafedf2985cfc
Tim Huerne <timothee.huerne@gmail.com>
2021-09-20 21:07:26 +0200

    first commit

---
+++ public/home.js
@@ -0,0 +1,12 @@
+function Home(){
+    return (
+      <Card
+        bgcolor="info"
+        txtcolor="black"
+        header="BadBank Landing Module"
+        title="Welcome to the bank"
+        text="You can move around using the navigation bar."
+        body={(<img src="bank.svg" className="img-fluid" alt="Responsive image"/>)}
+      />    
+    );  
+  }
